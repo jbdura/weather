a01e56a2eb7ec8bfa205ba347ee90c11bc4d90c1
jbdura <bidurajohnson@gmail.com>
2022-06-24 12:42:10 +0300

    code cleanup 1

--- src/index.js
+++ src/index.js
@@ -94,18 +94,6 @@ tempElement.addEventListener("click", function(){
 });
 
 
-// // Inject the time in the UI
-// var renderTime = function () {
-// 	var time = new Date();
-// 	clock.textContent = time.toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
-// };
-
-// // Render the time on load
-// renderTime();
-
-// // Update the time every second
-// setInterval(renderTime, 1000);
-
 function display_c(){
 	var refresh=1000; // Refresh rate in milli seconds
 	mytime=setTimeout('display_ct()',refresh)
