72dbc052ce8fac70dceff936f9a257e4df585a80
jbdura <bidurajohnson@gmail.com>
2022-06-24 12:16:55 +0300

    clock delete1

--- src/index.js
+++ src/index.js
@@ -94,14 +94,14 @@ tempElement.addEventListener("click", function(){
 });
 
 
-// Inject the time in the UI
-var renderTime = function () {
-	var time = new Date();
-	clock.textContent = time.toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
-};
+// // Inject the time in the UI
+// var renderTime = function () {
+// 	var time = new Date();
+// 	clock.textContent = time.toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+// };
 
-// Render the time on load
-renderTime();
+// // Render the time on load
+// renderTime();
 
-// Update the time every second
-setInterval(renderTime, 1000);
+// // Update the time every second
+// setInterval(renderTime, 1000);
