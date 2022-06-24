0f14bba7167d0cb4614a4233d963bbb284e921ac
jbdura <bidurajohnson@gmail.com>
2022-06-24 09:42:33 +0300

    js changes

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ const kelvin = 273
 // API
 const apiKey = "82005d27a116c2880c8f0fcb866998a0"
 
-// "c87292305339360852056b3204d4f50e"
 
 
 // geolocation
@@ -46,7 +45,6 @@ function showError(error) {
 // API
 function getWeather(latitude, longitude) {
 	let api = `https://api.openweathermap.org/data/2.5//weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
-	// `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&long=${longitude}&appid=${api}`
 
 	// console.log(api)
 	fetch(api)
